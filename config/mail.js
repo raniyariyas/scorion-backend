9c3ef85e4fa4157d92d6eb7b54f4b8e9c0987997
FASALGAFOOR <fasalgafoor2080@gmail.com>
2026-03-02 16:48:26 +0530

    changed email service to brevo

--- config/mail.js
+++ config/mail.js
@@ -4,21 +4,20 @@ dotenv.config();
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, 
+    host: "79.smtp-relay.brevo.com", // Updated for Brevo
+    port: 587,
+    secure: false, // Port 587 uses STARTTLS
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
     },
     tls: {
       rejectUnauthorized: false
-    },
-    connectionTimeout: 10000, // 10 seconds
-    greetingTimeout: 10000,
-    socketTimeout: 10000
+    }
   });
 };
+
+
 
 const baseTemplate = (content, name, subject) => `
 <!DOCTYPE html>
