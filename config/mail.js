ae13b73766d8bb3434816f9d71f1307621f7b06c
FASALGAFOOR <fasalgafoor2080@gmail.com>
2026-03-02 16:11:32 +0530

    changed email servicesss

--- config/mail.js
+++ config/mail.js
@@ -6,7 +6,7 @@ const createTransporter = () => {
   return nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, // Use SSL/TLS
+    secure: true, 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
