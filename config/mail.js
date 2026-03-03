8d3e97c1aec2229732e60d75f58185ff23531bd8
FASALGAFOOR <fasalgafoor2080@gmail.com>
2026-03-03 07:19:58 +0530

    bug on email

--- config/mail.js
+++ config/mail.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp-relay.brevo.com",
+    port: 587,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
